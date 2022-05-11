916c905a2c4721be415c6373bc596ab1d82d769b
DaDa <99330762+DadaOffl@users.noreply.github.com>
2022-05-11 11:09:26 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('Join', url=f'https://t.me/dada_linkz')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
